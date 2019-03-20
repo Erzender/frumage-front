ca524cd6acd0797413102929fdf77c341c08ff44
Erzender <lukas.parant@epitech.eu>
2019-03-20 13:21:05 +0100

    reactstrap

--- src/scenes/Root/components/App.js
+++ src/scenes/Root/components/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { Button } from 'reactstrap';
 import root from '../duck/reducer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootReducer = combineReducers({ root });
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
-const App = () => <div style={{ flex: 1 }}>hello</div>;
+const App = () => (
+  <div style={{ flex: 1 }}>
+    <Button>loul</Button>
+  </div>
+);
 
 export default () => (
   <Provider store={store}>
