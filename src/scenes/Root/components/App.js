59e3d251365675dfd6fb36b4b12047765fbdd083
Erzender <lukas.parant@epitech.eu>
2019-03-20 13:33:46 +0100

    router, persist

--- src/scenes/Root/components/App.js
+++ src/scenes/Root/components/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Button } from 'reactstrap';
 import root from '../duck/reducer';
+import persist from '../duck/persist';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const rootReducer = combineReducers({ root });
+const persistConfig = {
+  key: 'persist',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, persist);
+
+const rootReducer = combineReducers({ root, persistedReducer });
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const persistor = persistStore(store);
 
 const App = () => (
   <div style={{ flex: 1 }}>
@@ -17,6 +29,8 @@ const App = () => (
 
 export default () => (
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
